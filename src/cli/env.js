c0546d3c18f24935feb38a84cae6f45e51cc30ee
Davyd <wliorjroo@gmail.com>
2022-11-30 22:08:02 +0300

    feat: subtask hash, subtask cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
+import { env } from 'process'
+
 const parseEnv = () => {
-    // Write your code here 
+    Object.keys(env).forEach((key) => {
+		if (key.toString().startsWith('RSS_')) {
+			console.log(`${key}=${env[key]}`);
+		}
+	});
 };
 
 parseEnv();
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { readFile } from 'fs'
+import { createHash } from 'crypto'
+
 const calculateHash = async () => {
-    // Write your code here 
+	const path = new URL("files/fileToCalculateHashFor.txt", import.meta.url);
+    readFile(path, (err, dataBuffer) => {
+		if (err) throw err;
+
+		const hash = createHash('sha256');
+		hash.update(dataBuffer);
+		console.log(hash.digest('hex'));
+	});
 };
 
 await calculateHash();

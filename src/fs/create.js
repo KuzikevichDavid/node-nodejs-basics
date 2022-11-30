cffd4789c054ff7cf511460256be09adc3aecf65
DavydKuzikevich <wliorjroo@gmail.com>
2022-11-30 15:04:35 +0300

    feat: subtask fs:create, read

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,32 @@
+import { open, close, write } from "fs"
+import { Buffer } from 'buffer';
+
 const create = async () => {
-    // Write your code here 
+	const errOperationFailed = new Error("FS operation failed");
+	const path = new URL("files/fresh.txt", import.meta.url);
+
+	const buffer = Buffer.from('I am fresh and young');
+	
+	
+    open(path, 'wx', (err, fd) => {
+		if (err) {
+			if (err.code === 'EEXIST') {
+				throw errOperationFailed;
+			}
+
+			throw err;
+		}
+
+		try {
+			write(fd, buffer, (err, bytesWritten, buf) => {
+				if (err) throw err;
+			});
+		} finally {
+			close(fd, (err) => {
+				if (err) throw err;
+			});
+		}
+	});
 };
 
 await create();

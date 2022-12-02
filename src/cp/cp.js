40cf020e0a9f7f4bd618a2b938c716c239fc2fa1
DavydKuzikevich <wliorjroo@gmail.com>
2022-12-02 15:37:37 +0300

    feat: complete subtask child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import { spawn } from 'child_process'
+import { fileURLToPath } from 'url'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+	const path = new URL("files/script.js", import.meta.url);
+	const jsScript = spawn('node', [`${fileURLToPath(path)}`, '--propName', 'value']);
+	
+	process.stdin.pipe(jsScript.stdin);
+	jsScript.stdout.pipe(process.stdout);
+	console.log('To terminate, type "CLOSE"');
 };
 
 spawnChildProcess();
